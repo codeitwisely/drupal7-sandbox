b9227a384e8dc947403ba4a1c7af3527ea019af2
Jean-Michel Lavarenne <jeanmichel@berkano.local>
2014-09-15 09:41:42 -0400

    Theme example

---
+++ docroot/sites/all/modules/custom/app_theme/theme/app_theme.tpl.php
@@ -0,0 +1,12 @@
+<?php dpm($variables); ?>
+<div class="main-div">
+  <div class="first-friend"><?php print $first_friend; ?></div>
+  <div class="collector"><?php print $second_collector; ?></div>
+
+  <?php if ($amazing_variable): ?>
+
+    <p><?php print $amazing_variable; ?></p>
+  
+  <?php endif; ?>
+
+</div>
