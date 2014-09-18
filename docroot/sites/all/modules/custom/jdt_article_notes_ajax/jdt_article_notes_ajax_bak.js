07fce7e02f4b83d8e1f766fda186155b3ae66754
Chaudhry Usman Ali <chaudhryusmanali@Chaudhrys-MacBook-Air.local>
2014-09-18 16:26:42 -0400

    Jquery added - ajax sending back data to form to be saved in db

--- docroot/sites/all/modules/custom/jdt_article_notes_ajax/jdt_article_notes_ajax_bak.js
+++ docroot/sites/all/modules/custom/jdt_article_notes_ajax/jdt_article_notes_ajax_bak.js
@@ -3,13 +3,12 @@
 Drupal.behaviors.jdt_article_notes_ajax = {
   attach: function (context) {
    $('#editme').click(function() {
-     //alert('Yay');
      	// Add a div
      	var $div = $('<div />').appendTo('body');
 		$div.attr('id', 'holdy');
 		$("#holdy").dialog();
 		var nodeid = Drupal.settings.jdt_article_notes_theming.nid;
-		$('#holdy').load('/note/'+ nodeid);
+    $("#holdy").load('/sendmessage/'+nodeid+'?ajax');
   	});
   }
 };
--- docroot/sites/all/modules/custom/jdt_article_notes_ajax/theme/jdt_article_notes_ajax.tpl.php
+++ docroot/sites/all/modules/custom/jdt_article_notes_ajax/theme/jdt_article_notes_ajax.tpl.php
@@ -1,4 +1,4 @@
-<?php //dpm($variables); 
+<?php //dpm($variables);
 ?>
 
 <!-- Tab template -->
@@ -7,6 +7,7 @@
   <div class="node-title"><?php print $NODE_TITLE; ?></div>
   <div class="node-author"><?php print $NODE_AUTHOR; ?></div>
   <div class="node-load"></div>
+  <input type="text" value="" name="name" id="my-name" style="display:none">
   <div class="send-message-dialog"</div>
   <button class="send_message">Edit Note</button>
 </div>
